9ccf5aea06e15700162075d16de71cf2f91ea62f
naki1993 <168444448+naki1993@users.noreply.github.com>
2024-08-02 21:42:24 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    $user_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+
+    print_r($user_data);
+    ?>
+    </p>
+</body>
+</html>
